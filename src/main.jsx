88e8d5facde41df9fc45e5faf4fb31c33412321d
Jamie Holding <me@jamie.holdings>
2022-05-12 08:29:05 +0100

    [+] Basic cloudflare setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <h1>Test Cloudflare Deployment</h1>
+  </React.StrictMode>
+);
